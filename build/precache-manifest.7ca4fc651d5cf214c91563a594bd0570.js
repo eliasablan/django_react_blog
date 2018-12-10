13ccc2d36d0301795581753f53007a7cc65b7f4e
Elias <eliasablan93@gmail.com>
2018-12-09 21:09:17 -0400

    Configuracion final para hacer Deploy

---
+++ build/precache-manifest.7ca4fc651d5cf214c91563a594bd0570.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb01ee9fbdf77cc740d4",
+    "url": "/static/js/main.bb01ee9f.chunk.js"
+  },
+  {
+    "revision": "3022c96a3388e5f38659",
+    "url": "/static/css/1.8ee45876.chunk.css"
+  },
+  {
+    "revision": "3022c96a3388e5f38659",
+    "url": "/static/js/1.3022c96a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a79038d07e10115623e61b07fc6461c6",
+    "url": "/index.html"
+  }
+];
